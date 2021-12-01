d93e98aef3d9f546ced2e935afc1d946384fdc12
SanchezD1998 <44213289+SanchezD1998@users.noreply.github.com>
2021-12-01 18:34:47 -0500

    Part 1

--- app_persistencia_sqflite/lib/main.dart
+++ app_persistencia_sqflite/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:app_persistencia_sqflite/util/dbhelper.dart';
+import 'package:app_persistencia_sqflite/models/shopping_list.dart';
+import 'package:app_persistencia_sqflite/models/list_items.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,16 +10,57 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    DbHelper helper = DbHelper();
-    helper.testDB();
-
+    //DbHelper helper = DbHelper();
+    //helper.testDB();
     return MaterialApp(
       title: "Lista de compras",
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Container(
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My shopping list!!!"),
+        ),
+        body: ShowList(),
       ),
     );
+  }
+}
+
+class ShowList extends StatefulWidget {
+  @override
+  _ShowListState createState() => _ShowListState();
+}
+
+class _ShowListState extends State<ShowList> {
+  DbHelper helper = DbHelper();
+  List<ShoppingList> shoppingList=[];
+  @override
+  Widget build(BuildContext context) {
+    showData();
+    return ListView.builder(
+        itemCount: (shoppingList != null)? shoppingList.length : 0,
+          itemBuilder:(BuildContext context, int index){
+          return ListTile(
+            title: Text(shoppingList[index].name),
+            leading: CircleAvatar(
+              child: Text(shoppingList[index].priority.toString()),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: (){
+
+              },
+            ),
+          );
+        }
+    );
+  }
+
+  Future showData() async{
+    await helper.openDb();
+    shoppingList = await helper.getLists();
+    setState(() {
+      shoppingList = shoppingList;
+    });
   }
 }
 
--- app_persistencia_sqflite/lib/util/dbhelper.dart
+++ app_persistencia_sqflite/lib/util/dbhelper.dart
@@ -1,40 +1,62 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:app_persistencia_sqflite/models/shopping_list.dart';
+import 'package:app_persistencia_sqflite/models/list_items.dart';
 
 class DbHelper {
   final int version = 1;
-  var db;
+  Database? db;
 
-  Future<Database> openDb() async {
-    if (db == null) {
-      db = await openDatabase(join(await getDatabasesPath(), 'shopping_v2.db'),
+  Future<Database> openDb() async{
+    if (db == null)
+    {
+      db = await openDatabase(join(await getDatabasesPath(), 'shopping_v12.db'),
           onCreate: (database, version) {
-        database.execute('CREATE TABLE lists('
-            'id INTEGER PRIMARY KEY,'
-            'name TEXT,'
-            'priority INTEGER)');
-        database.execute('CREATE TABLE items('
-            'id INTEGER PRIMARY KEY,'
-            'idList INTEGER,'
-            'name TEXT,'
-            'quantity TEXT,'
-            'note TEXT,'
-            'FOREING KEY(idlist) REFERENCES lists(id))');
+          database.execute('CREATE TABLE lists(id INTEGER PRIMARY KEY, name TEXT, priority INTEGER)');
+          database.execute('CREATE TABLE items(id INTEGER PRIMARY KEY, idList INTEGER, name TEXT, quantity TEXT, note TEXT, FOREIGN KEY(idlist) REFERENCES lists(id))');
       }, version: version);
     }
-    return db;
+    return db!;
   }
 
   Future testDB() async {
     db = await openDb();
-
-    await db.execute('INSERT INTO lists VALUES (0, "Memorias", 1)');
-    await db.execute('INSERT INTO items VALUES (0, 0, "Memorias USB", "20 unds.", "Marca Kingstone")');
-
-    List list = await db.rawQuery('SELECT * FROM lists');
-    List item = await db.rawQuery('SELECT * FROM items');
+    await db!.execute('INSERT INTO lists VALUES (0, "Discos duros", 1)');
+    await db!.execute('INSERT INTO items VALUES (0, 0, "SSD", "55 unds.", "Marca WD")');
+    List list = await db!.rawQuery('SELECT * FROM lists');
+    List item = await db!.rawQuery('SELECT * FROM items');
 
     print(list[0]);
     print(item[0]);
+  }
+
+  //insert list
+  Future<int> insertList(ShoppingList list) async {
+    int id = await this.db!.insert('lists', list.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+
+    return id;
+  }
+
+  //insertar en tabla items
+  Future<int> insertItem(ListItem item) async {
+    int id = await this.db!.insert(
+        'items',
+        item.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace); // obligatorio
+
+    return id;
+  }
+
+  //metodo para mostrar la tabla "lists"
+  Future<List<ShoppingList>> getLists() async {
+    final List<Map<String, dynamic>> maps = await db!.query('lists');
+    return List.generate(maps.length, (i) {
+      return ShoppingList(
+        maps[i]['id'],
+        maps[i]['name'],
+        maps[i]['priority'],
+      );
+    });
   }
 }
